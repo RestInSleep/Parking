ad2be036b5fbbe4cd6d6652752a4806a09061d57
Jan <jasjago@gmail.com>
2023-10-08 13:46:18 +0200

    funkcja na sprawdzenie inuptu

--- Parking/main.cpp
+++ Parking/main.cpp
@@ -1,4 +1,15 @@
 #include <iostream>
+#include <string>
+#include <regex>
+
+
+bool isPlateNameGood(std::string& plate) {
+    
+    
+    
+    return std::regex_match(plate, std::regex("\s*([A-Z][A-Z0-9]{2,10})\s*((0?[89]|1[0-9])\.([0-5][0-9])|20\.00)\s*((0?[89]|1[0-9])\.([0-5][0-9])|20\.00)\s*"));
+}
+
 
 
 int main() {
