2ebb1270a494d7a4c420a37fbb3a9ded0da0073d
smoczy123 <michal.hudobski@poczta.onet.pl>
2023-10-10 20:16:34 +0200

    changed weird tuple to pair of int and bool

--- Parking/main.cpp
+++ Parking/main.cpp
@@ -2,11 +2,12 @@
 #include <regex>
 #include <string>
 #include <unordered_map>
+#include <utility>
 
 
 
-using carBase = std::unordered_map<std::string, std::tuple<int, int, int,int>>;
-//start day, start time, end day, end time
+using carBase = std::unordered_map<std::string, std::pair<int, bool>>;
+//end time, does it end in the next day
 
 int time_string_to_int(std::string time) {
     int time_in_minutes{};
@@ -20,16 +21,16 @@ int time_string_to_int(std::string time) {
 }
 
 
-void removeOutdatedCars(carBase& cars, int current_day,
-                        int current_time) {
+void removeOutdatedCars(carBase& cars) {
     
     std::vector<std::string> toRemove{};
     for (auto c : cars) {
-        int end_day = std::get<2>(c.second);
-        int end_time = std::get<3>(c.second);
-        if (current_day > end_day ||(current_day == end_day && current_time > end_time)) {
-            toRemove.push_back(c.first);
-        }
+			if (!c.second.second) {
+				toRemove.push_back(c.first);
+			}
+			else {
+				c.second.second = false;
+			}
     }
     for (std::string name : toRemove) {
         cars.erase(name);
@@ -49,7 +50,6 @@ int main() {
     const std::regex add_request{"\\s*([A-Z][A-Z0-9]{2,10})\\s*((?:0?[89]|1[0-9])\\.(?:[0-5][0-9])|20\\.00)\\s*((?:0?[89]|1[0-9])\\.(?:[0-5][0-9])|20\\.00)\\s*"};
     
     int line_number{1};
-    int current_day{1};
     int current_time{};
     std::string line;
     while(std::getline(std::cin, line)) {
@@ -60,22 +60,19 @@ int main() {
             
             int begin = time_string_to_int(match[2]);
             if (current_time > begin) {
-                current_day++;
+							removeOutdatedCars(cars);
             }
             current_time = begin;
-            
+            bool does_it_end_tomorrow = false;
             int end = time_string_to_int(match[3]);
-            int start_day = current_day;
-            
-            int end_day = current_day;
             if (end < begin) {
-                end_day++;
+                does_it_end_tomorrow = true;
             }
             
             if(atLeastTenMinutes(begin, end)) {
                 std::cout << "OK " << line_number << '\n';
-                std::tuple<int, int, int, int> timeData =
-                {start_day, begin, end_day, end};
+                std::pair<int, bool> timeData =
+                {end, does_it_end_tomorrow};
                 cars.insert(std::make_pair(name, timeData));
             }
             else {
@@ -87,7 +84,7 @@ int main() {
             std::string name = match[1];
             int check_time = time_string_to_int(match[2]);
             if(check_time < current_time) {
-                current_day++;
+							removeOutdatedCars(cars);
             }
             current_time = check_time;
             int is_the_car_in_system = static_cast<int>(cars.count(name));
@@ -95,13 +92,13 @@ int main() {
                 std::cout << "NO " << line_number << '\n';
             }
             else {
-                int end_day = std::get<2>(cars[name]);
-                int end_time = std::get<3>(cars[name]);
-                if (current_day > end_day || (current_day == end_day && current_time > end_time)) {
-                    std::cout << "NO " << line_number << '\n';
+                int end_time = cars[name].first;
+								bool does_it_end_tomorrow = cars[name].second;
+                if (does_it_end_tomorrow || current_time < end_time) {
+                    std::cout << "YES " << line_number << '\n';
                 }
                 else {
-                    std::cout << "YES " << line_number << '\n';
+                    std::cout << "NO " << line_number << '\n';
                 }
             }
         }
@@ -109,7 +106,6 @@ int main() {
             std::cerr << "ERROR " << line_number << '\n';
         }
         line_number++;
-        removeOutdatedCars(cars, current_day, current_time);
     }
     return 0;
     
